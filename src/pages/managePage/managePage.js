f1b7e7cefeb783dbc13138b9f8a6709db9c5c586
Andrew Southwick <Andrew.Southwick@us.usana.com>
2019-08-29 18:54:48 -0600

    function in managePage

--- src/pages/managePage/managePage.js
+++ src/pages/managePage/managePage.js
@@ -2,14 +2,36 @@ import React, {Component} from 'react'
 import SideNav from '../../components/sideNav'
 import PropTypes from 'prop-types'
 class ManagePage extends Component {
+    state = {
+        name: '',
+        addNumber: 1
+    }
+
     deleteClicked = () => {
         console.log('deleted')
     }
+    addNumber = (number) => {
+        this.state.addNumber++
+      
+       this.setState({
+           addNumber: this.state.addNumber++
+       })
+    }
     render(){
+        console.log(this.state)
         return(
+           
             <div>
+                {this.state.name}
                 <SideNav title={"this is a title"} rating="5" allowDelete={true} click={this.deleteClicked}/>
-                {this.props.title}
+              
+                <button onClick={()=>{this.addNumber()}}>add the number</button>
+               
+                <input type="text"  onChange={(e) => {
+                    this.setState({
+                        name: e.target.value
+                    })
+                }}/>
             </div>
         )
     }
